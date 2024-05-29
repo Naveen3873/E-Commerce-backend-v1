a948777d53c9230f731f0170df70dae1dae269ee
Naveen3873 <naveenbe3873@gmail.com>
2024-05-30 00:24:07 +0530

    Create a nodejs project for E-Commerce and monogdb connect model create api router check successfully

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection is Successfully..."))
+    .catch((err) => console.log(err));
+
+// app.get('/api/test', () => {
+//     console.log("The api for testing");
+// })
+
+app.use(express.json());
+app.use('/api/users', userRoute);
+
+// const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT || 5000, () => {
+    console.log('The Nodejs server is running...');
+})
---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get("/test",(req,res) => {
+    res.send("The user test api calling...")
+});
+
+router.post("/userCheck",(req,res) => {
+    const { username } = req.body;
+    res.send(`Your username is ${username}`);
+});
+
+module.exports = router;
