be096782821a6dd5cf4ab3945edf684da5cd1c26
Naveen3873 <naveenbe3873@gmail.com>
2024-05-30 07:58:40 +0530

    user register and login api created and user register password with encrypt crypto-js npm used

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
-router.get("/test",(req,res) => {
-    res.send("The user test api calling...")
-});
+// router.get("/test",(req,res) => {
+//     res.send("The user test api calling...")
+// });
 
-router.post("/userCheck",(req,res) => {
-    const { username } = req.body;
-    res.send(`Your username is ${username}`);
-});
+// router.post("/userCheck",(req,res) => {
+//     const { username } = req.body;
+//     res.send(`Your username is ${username}`);
+// });
 
 module.exports = router;
